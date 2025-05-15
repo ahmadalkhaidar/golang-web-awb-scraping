5babf6c9f5d6e7b62ad8c98dd7024831af12f527
Ahmad Al Khaidar <ahmadalkhaidar2@gmail.com>
2025-05-15 10:17:18 +0700

    update data format json response

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -16,14 +16,19 @@ type Status struct {
 	Message string
 }
 
+type Formatted struct {
+	CreatedAt string `json:"createdat"`
+}
+
 type TrackingInfo struct {
-	Description string `json:"description"`
-	Timestamp   string `json:"timestamp"`
+	Description string      `json:"description"`
+	Timestamp   string      `json:"timestamp"`
+	Formatted   []Formatted `json:"formatted"`
 }
 
 type AWBData struct {
-	WaybillNumber string         `json:"waybill_number"`
-	History       []TrackingInfo `json:"history"`
+	ReceivedBy string         `json:"receivedBy"`
+	Histories  []TrackingInfo `json:"histories"`
 }
 
 func ScrapeAWB(awb string) (*AWBData, error) {
@@ -65,25 +70,42 @@ func ScrapeAWB(awb string) (*AWBData, error) {
 	doc.Find(".history-details tr").Each(func(i int, s *goquery.Selection) {
 		tds := s.Find("td")
 		if tds.Length() >= 2 {
-			desc := strings.TrimSpace(tds.Eq(0).Text())
-			timestamp := strings.TrimSpace(tds.Eq(1).Text())
-
+			desc := strings.TrimSpace(tds.Eq(1).Text())
+			timestamp := strings.TrimSpace(tds.Eq(0).Text())
 			if desc != "" && timestamp != "" {
+				createdat := timestamp
+				// Parsing dari string ke time.Time
+				inputTimeStamp := timestamp
+				t, err := time.Parse("02-01-2006 15:04", inputTimeStamp)
+				if err == nil {
+					// Format kembali jadi string
+					createdat = t.Format("02 January 2006, 15:04 WIB")
+				}
+
 				history = append(history, TrackingInfo{
 					Description: desc,
 					Timestamp:   timestamp,
+					Formatted: []Formatted{
+						{
+							CreatedAt: createdat,
+						},
+					},
 				})
+
 			}
 		}
 	})
+
+	trConsignee := doc.Find(".shipper-details tr").Eq(1)
+	consignee := trConsignee.Find("td").Eq(1).Text()
 
 	if len(history) == 0 {
 		return nil, Response("404", "AWB number not found")
 	}
 
 	return &AWBData{
-		WaybillNumber: awb,
-		History:       history,
+		ReceivedBy: consignee,
+		Histories:  history,
 	}, nil
 }
 
